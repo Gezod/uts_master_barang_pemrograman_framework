5cda9e331817e1f0e06e94dd73d03396041c64ac
Gezod <33573930+Gezod@users.noreply.github.com>
2024-05-05 14:08:37 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,6 +60,8 @@
                                     <p><strong>Tempat, Tanggal Lahir:</strong> Surabaya, 8 Juni 2001</p>
                                     <p><strong>Usia:</strong> 23 Tahun</p>
                                     <p><strong>Jenis Kelamin: </strong> Pria</p>
+                                    <p><strong>Jurusan </strong> Sistem Informasi</p>
+                                    <p><strong>NIM </strong> 1204220137</p>
                                     <p><strong>Alamat Email: </strong> refanggalintar@student.telkomuniversity.ac.id</p>
                                     <p><strong>Motto: </strong> Saya adalah orang yang sangat disiplin dan fokus terhadap hasil kerja hingga cenderung idealis. Namun, saya juga bisa realistis saat menentukan target atau tujuan dan selalu berupaya keras untuk bisa mencapai tujuan tersebut dengan cara yang baik dan efisien</p>
                                 </div>
